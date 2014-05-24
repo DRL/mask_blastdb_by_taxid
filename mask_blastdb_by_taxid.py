8141fdb1c421e18cb476c6ad293acc6088083e8d
Dominik R. Laetsch <dominik.laetsch@gmail.com>
2014-05-24 21:13:03 +0100

    debug

--- mask_blastdb_by_taxid.py
+++ mask_blastdb_by_taxid.py
@@ -105,7 +105,7 @@ def output_gis(db, gis_of_taxid, out_suffix, merge_flag):
 def make_alias_blastdbs(db, gi_filenames):
 	'''Makes alias blastdbs of db based on gi lists in output_filenames and returns the paths for the alias blastdbs''' 
 	for gi_filename in gi_filenames:
-		subprocess.call("blastdb_aliastool -gilist " + gi_filename + " -db " + db.filename + " -out " + gi_filename + "db", shell=True)
+		subprocess.call("blastdb_aliastool -gilist " + gi_filename + " -db " + db.filename + "-dbtype " + db.type + " -out " + gi_filename + "db", shell=True)
 		#try:
 			#call(["module load blast"]) # for bigfoot
 			
